58192f9b131c9a8c21213066359e432eb29383eb
PRADEEPSINHCHAVDA <19it026@charusat.edu.in>
2021-06-03 11:16:12 +0530

    ADD: login and signup page

--- lib/views/login_page.dart
+++ lib/views/login_page.dart
@@ -17,136 +17,136 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       backgroundColor: theme.backgroundColor,
       body: SafeArea(
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                height: size.height * 0.1,
-                width: size.width,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: size.height * 0.1,
+              width: size.width,
+            ),
+            Text(
+              'Welcome Back!',
+              style: TextStyle(
+                fontSize: 24.0,
+                fontWeight: FontWeight.bold,
               ),
-              Text(
-                'Welcome Back!',
-                style: TextStyle(
-                  fontSize: 24.0,
-                  fontWeight: FontWeight.bold,
-                ),
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Text(
+              'Please sign in to your account',
+              style: TextStyle(
+                fontSize: 12.0,
+                color: Colors.grey,
+                fontWeight: FontWeight.bold,
               ),
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Text(
-                'Please sign in to your account',
+            ),
+            SizedBox(
+              height: size.height * 0.1,
+            ),
+            StyledTextField(
+              hintText: 'Email',
+            ),
+            StyledTextField(
+              hintText: 'Password',
+              isObscure: isObscure,
+              trailing: GestureDetector(
+                  onTap: () {
+                    setState(() {
+                      isObscure = !isObscure;
+                    });
+                  },
+                  child: Icon(
+                    isObscure ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.grey,
+                  )),
+            ),
+            SizedBox(
+              height: size.height * 0.02,
+            ),
+            Container(
+              width: size.width * 0.85,
+              child: Text(
+                'Forget Password?',
+                textAlign: TextAlign.right,
                 style: TextStyle(
                   fontSize: 12.0,
                   color: Colors.grey,
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              SizedBox(
-                height: size.height * 0.1,
+            ),
+            Expanded(
+              child: SizedBox(
+
               ),
-              StyledTextField(
-                hintText: 'Email',
+            ),
+            StyledButton(
+              'Sign In',
+              status: true,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Homepage(),
+                  ),
+                );
+              },
+            ),
+            StyledButton(
+              'Sign In with Google',
+              status: true,
+              onPressed: () {},
+              color: theme.buttonColor,
+              fontColor: theme.accentColor,
+              leading: Padding(
+                padding: const EdgeInsets.only(right: 12.0),
+                child: Image.network(
+                  'https://img.icons8.com/fluent/48/000000/google-logo.png',
+                  width: 26.0,
+                ),
               ),
-              StyledTextField(
-                hintText: 'Password',
-                isObscure: isObscure,
-                trailing: GestureDetector(
-                    onTap: () {
-                      setState(() {
-                        isObscure = !isObscure;
-                      });
-                    },
-                    child: Icon(
-                      isObscure ? Icons.visibility_off : Icons.visibility,
-                      color: Colors.grey,
-                    )),
-              ),
-              SizedBox(
-                height: size.height * 0.02,
-              ),
-              Container(
-                width: size.width * 0.85,
-                child: Text(
-                  'Forget Password?',
-                  textAlign: TextAlign.right,
+            ),
+            SizedBox(
+              height: size.height * 0.04,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Don\'t have an Account? ',
                   style: TextStyle(
                     fontSize: 12.0,
-                    color: Colors.grey,
+                    color: theme.buttonColor,
                     fontWeight: FontWeight.bold,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              StyledButton(
-                'Sign In',
-                status: true,
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Homepage(),
-                    ),
-                  );
-                },
-              ),
-              StyledButton(
-                'Sign In with Google',
-                status: true,
-                onPressed: () {},
-                color: theme.buttonColor,
-                fontColor: theme.accentColor,
-                leading: Padding(
-                  padding: const EdgeInsets.only(right: 12.0),
-                  child: Image.network(
-                    'https://img.icons8.com/fluent/48/000000/google-logo.png',
-                    width: 26.0,
-                  ),
-                ),
-              ),
-              SizedBox(
-                height: size.height * 0.04,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Don\'t have an Account? ',
+                GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => SignUpPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    'Sign Up',
                     style: TextStyle(
                       fontSize: 12.0,
-                      color: theme.buttonColor,
+                      color: Colors.blue,
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  GestureDetector(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignUpPage(),
-                        ),
-                      );
-                    },
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        fontSize: 12.0,
-                        color: Colors.blue,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(
-                height: size.height * 0.06,
-              ),
-            ],
-          ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: size.height * 0.06,
+            ),
+          ],
         ),
       ),
     );
--- lib/views/signup_page.dart
+++ lib/views/signup_page.dart
@@ -1,11 +1,139 @@
 import 'package:flutter/material.dart';
+import 'package:supachat/views/home_page.dart';
 
-class SignUpPage extends StatelessWidget {
+import 'package:supachat/widgets/styled_widgets.dart';
+
+class SignUpPage extends StatefulWidget {
+
+  @override
+  _SignUpPageState createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  bool isObscure = true;
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+
+    final ThemeData theme = Theme.of(context);
     return Scaffold(
       backgroundColor: Theme.of(context).backgroundColor,
-      body: Center(child: Text('Add Sign Up Page here')),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(
+                height: size.height * 0.1,
+                width: size.width,
+              ),
+              Text(
+                'Create new Account',
+                style: TextStyle(
+                  fontSize: 24.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Text(
+                'Please fill in the form to continue',
+                style: TextStyle(
+                  fontSize: 12.0,
+                  color: Colors.grey,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.1,
+              ),
+              StyledTextField(
+                hintText: 'Full Name',
+              ),
+
+              StyledTextField(
+                hintText: 'Email Address',
+              ),
+
+              StyledTextField(
+                hintText: 'Phone Number ',
+              ),
+
+              StyledTextField(
+                hintText: 'Password',
+                isObscure: isObscure,
+                trailing: GestureDetector(
+                    onTap: () {
+
+                      setState(() {
+                        isObscure = !isObscure;
+                      });
+                    },
+                    child: Icon(
+                      isObscure ? Icons.visibility_off : Icons.visibility,
+                      color: Colors.grey,
+                    )),
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              StyledButton(
+                'Sign Up',
+                status: true,
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => Homepage(),
+                    ),
+                  );
+                },
+              ),
+
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Have an Account? ',
+                    style: TextStyle(
+                      fontSize: 12.0,
+                      color: theme.buttonColor,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.06,
+              ),
+            ],
+          ),
+        ),
+
+
+      ),
     );
   }
 }
